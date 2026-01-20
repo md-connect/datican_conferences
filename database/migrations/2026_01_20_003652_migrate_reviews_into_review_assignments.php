d8f06338a3959865feb74bf4e759cd8f3fe5fdf6
Monday Oke <mondayoke93@gmail.com>
2026-01-20 19:38:34 +0100

    CMT implementation

--- database/migrations/2026_01_20_003652_migrate_reviews_into_review_assignments.php
+++ database/migrations/2026_01_20_003652_migrate_reviews_into_review_assignments.php
@@ -3,10 +3,10 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
-    use Illuminate\Support\Facades\DB;
 
     public function up()
     {
